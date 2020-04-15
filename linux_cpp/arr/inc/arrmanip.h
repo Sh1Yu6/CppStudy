6d0747fb7cefcc9bb15e44e559e21009070352ff
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-04-15 20:58:21 +0800

    a

---
+++ linux_cpp/arr/inc/arrmanip.h
@@ -0,0 +1,11 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+
+void generate_integers(int a[], unsigned int n, int lower, int upper);
+
+void reverse_integers(int a[], unsigned int n);
+
+void swap_integers(int a[], unsigned int i, unsigned int j);
+
+void print_integers(int a[], unsigned int n);
+#endif

495bb2523729f834ebd8913e5d25c4527d37ebcd
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-04-15 19:05:58 +0800

    a

---
+++ linux_cpp/make_file/func1.cpp
@@ -0,0 +1,21 @@
+
+/************************************************
+#
+#      Filename: func1.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-04-15 13:41:37
+# Last Modified: 2020-04-15 13:44:12
+#***********************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include "func1.h"
+void func()
+{
+    std::cout << "hello world!" << "\n";
+}
---
+++ linux_cpp/make_file/main.cpp
@@ -0,0 +1,22 @@
+
+/************************************************
+#
+#      Filename: main.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-04-15 13:41:18
+# Last Modified: 2020-04-15 13:44:25
+#***********************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include "func1.h"
+int main(int argc, char *argv[])
+{
+    func();
+    return 0;
+}

7ec1666396c3bb2a69adebd13299ab005b2db7c9
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-04-25 21:46:34 +0800

    a

---
+++ cpp程序设计原理与实践/chapter13/test2.cpp
@@ -0,0 +1,28 @@
+
+/************************************************
+#
+#      Filename: test2.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-04-25 19:42:35
+# Last Modified: 2020-04-25 19:50:06
+#***********************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include <cstring>
+int main(int argc, char *argv[])
+{
+
+    char p[100];
+    std::cin.width(100);
+    std::cin >> p;
+
+    std::cout << strlen(p);
+
+    return 0;
+}

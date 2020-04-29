aeb03e71bee1aedc28d61019cd0e673c25c3bf67
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-04-29 19:10:25 +0800

    a

---
+++ cpp程序设计原理与实践/chapter15/test1.cpp
@@ -0,0 +1,23 @@
+
+/************************************************
+#
+#      Filename: test1.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-04-26 20:25:24
+# Last Modified: 2020-04-29 19:10:12
+#***********************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+int main(int argc, char *argv[])
+{
+    std::cout << "a\n";
+
+
+    return 0;
+}

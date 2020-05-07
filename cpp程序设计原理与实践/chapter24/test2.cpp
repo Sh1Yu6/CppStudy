3c7a6f6cbf32982050404f48430c7c4afc8f80ca
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-05-07 10:59:39 +0800

    a

---
+++ cpp程序设计原理与实践/chapter24/test2.cpp
@@ -0,0 +1,23 @@
+
+/************************************************
+#
+#      Filename: test2.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-05-06 19:23:52
+# Last Modified: 2020-05-06 19:24:13
+#***********************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include <matrix>
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
---
+++ cpp程序设计原理与实践/chapter24/test3.cpp
@@ -0,0 +1,24 @@
+
+/************************************************
+#
+#      Filename: test3.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-05-06 20:46:42
+# Last Modified: 2020-05-06 20:47:18
+#***********************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+void func()
+{
+    std::cout << "asdf\n";
+}
+int main(int argc, char *argv[])
+{
+    return 0;
+}

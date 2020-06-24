460ea8db93d674090d711e26c412720b21cde576
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-06-24 20:46:28 +0800

    a

---
+++ hei/third/test4.cpp
@@ -0,0 +1,65 @@
+
+/************************************************
+#
+#      Filename: test4.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-06-23 20:38:52
+# Last Modified: 2020-06-23 21:02:20
+#***********************************************/
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+class Worker
+{
+    public:
+        string mName;
+        int mSalary;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
+

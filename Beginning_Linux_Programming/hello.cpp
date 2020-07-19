fd4b8989212054215baa690be0046e85394d9eff
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-07-19 19:08:35 +0800

    a

---
+++ Beginning_Linux_Programming/hello.cpp
@@ -0,0 +1,18 @@
+
+/************************************************
+#
+#      Filename: hello.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-07-19 15:26:35
+# Last Modified: 2020-07-19 15:26:55
+#***********************************************/
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    cout << "hey" << endl;
+    return 0;
+}
+
---
+++ understanding_unix_linux_programming/more/more.cpp
@@ -0,0 +1,26 @@
+
+/************************************************
+#
+#      Filename: more.cpp
+#
+#        Author: Sh1Yu6
+#   Description: ---
+#        Create: 2020-07-18 14:59:42
+# Last Modified: 2020-07-18 15:22:11
+#***********************************************/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+const int kPageLen = 24;
+const int kLineLen = 512;
+
+void doMore(ifstream& in);
+int seeMore(ifstream& in);
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
+
